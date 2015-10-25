5c8b86dba102ecdc04784e0aa01e9c3aeaf7bb78
Xavier Carriba <xavier.carriba@gmail.com>
2015-10-25 22:58:15 +0000

    Create a inherintance structure for files managed by the system

---
+++ src/main/java/org/earelin/ecclesia/domain/resources/ManagedFile.java
@@ -0,0 +1,45 @@
+package org.earelin.ecclesia.domain.resources;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+/**
+ *
+ */
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class ManagedFile {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String mime;
+    private Date created;
+
+    public String getMime() {
+        return mime;
+    }
+
+    public void setMime(String mime) {
+        this.mime = mime;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
--- src/main/java/org/earelin/ecclesia/domain/resources/ManagedImage.java
+++ src/main/java/org/earelin/ecclesia/domain/resources/ManagedImage.java
@@ -1,24 +1,30 @@
 package org.earelin.ecclesia.domain.resources;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 /**
  * Managed images
  * Images managed by the system
  */
 @Entity
-public class ManagedImage {
-    @Id
-    @GeneratedValue
-    private long id;
+public class ManagedImage extends ManagedFile {
+    private int width;
+    private int height;
 
-    public long getId() {
-        return id;
+    public int getWidth() {
+        return width;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public void setWidth(int width) {
+        this.width = width;
     }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
 }
