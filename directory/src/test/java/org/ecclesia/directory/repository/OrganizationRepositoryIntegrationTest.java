3799e73563eed72711ce88ffc38c4b60b43af024
Xavier Carriba <xavier.carriba@gmail.com>
2019-02-10 10:25:36 +0000

    Managing dirty contexts

--- directory/src/test/java/org/ecclesia/directory/repository/OrganizationRepositoryIntegrationTest.java
+++ directory/src/test/java/org/ecclesia/directory/repository/OrganizationRepositoryIntegrationTest.java
@@ -6,14 +6,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
 public class OrganizationRepositoryIntegrationTest {
 
   @Autowired
